417be0cb5c230f41fe27eea0ac90869cfe4a04a4
Madlyfox <borromee.stanley@gmail.com>
2022-09-28 13:45:05 +0200

    first commit

---
+++ modal.js
@@ -0,0 +1,122 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const modalClose = document.querySelectorAll(".close");
+const formData = document.querySelectorAll(".formData");
+
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const radios = document.getElementsByName("location");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+modalClose.forEach((span) => span.addEventListener("click", closeModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+//FORM VALIDATION
+
+//Show input error messages
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//check location
+
+function checkLocation(input) {
+  for (var i = 0, length = input.length; i < length; i++) {
+    if (input[i].checked) {
+      showSucces(input[i]);
+
+      break;
+    } else {
+      showError(input[i], "Location is required");
+    }
+  }
+}
+
+//show success colour
+function showSucces(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+
+//check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSucces(input);
+  } else {
+    showError(input, "Email is not invalid");
+  }
+}
+
+//checkRequired fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSucces(input);
+    }
+  });
+}
+
+//check input Length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be les than ${max} characters`
+    );
+  } else {
+    showSucces(input);
+  }
+}
+
+//get FieldName
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//Event Listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkRequired([firstname, email, lastname, birthdate, quantity]);
+  checkLength(firstname, 2, 15);
+  checkLength(lastname, 2, 25);
+  checkLocation(radios);
+  checkEmail(email);
+});
